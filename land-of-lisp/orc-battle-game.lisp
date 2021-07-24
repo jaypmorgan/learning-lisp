4d82a51777eb15d7cc5ea1258179113ba2fce7e6
Jay Morgan <jaymiles17@gmail.com>
2021-07-24 21:44:08 +0100

    Finish adding the different monsters learning defmethod and defstruct

--- land-of-lisp/orc-battle-game.lisp
+++ land-of-lisp/orc-battle-game.lisp
@@ -16,7 +16,7 @@
 ;; defin a function called orc-batlle. Initialise the monsters and
 ;; start the game loop. Determine the victor after the game has ended
 ;; and print the ending messages.
-(defun orc-batlle ()
+(defun orc-battle ()
   (init-monsters)
   (init-player)
   (game-loop)
@@ -101,6 +101,8 @@
 	      m)))))
 
 ;; Monster management functions
+(defstruct monster (health (randval 10)))
+
 (defun init-monsters ()
   (setf *monsters* (map 'vector (lambda (x)
 				  (declare (ignore x))
@@ -131,8 +133,6 @@
 			    (monster-show m))))
 	 *monsters*)))
 
-(defstruct monster (health (randval 10)))
-
 (defmethod monster-hit (m x)
   (decf (monster-health m) x)
   (if (monster-dead m)
@@ -151,4 +151,86 @@
 
 (defmethod monster-attack (m))
 
+;; The wicked orc! Delivers a strong attack with his club, but
+;; otherwise pretty harmless. Every orc has a unique attack lever.
+(defstruct (orc (:include monster))	; inherit from basic monster
+  (club-level (randval 8)))
+(push #'make-orc *monster-builders*)
 
+(defmethod monster-show ((m orc))
+  (princ "A wicked orc with a level ")
+  (princ (orc-club-level m))
+  (princ " club"))
+
+(defmethod monster-attack ((m orc))
+  (let ((x (randval (orc-club-level m))))
+    (princ "An orc swings his club at your and knocks off ")
+    (princ x)
+    (princ " of your health points. ")
+    (decf *player-health* x)))
+
+;; The malicious hydra! A very nasty enemy. It will attack you with
+;; its many heads. The hydr's special power is that it can grow a new
+;; head during each round of battle, meaning you'll want to deal with
+;; it as quickly as possible.
+(defstruct (hydra (:include monster)))
+(push #'make-hydra *monster-builders*)
+
+(defmethod monster-show ((m hydra))
+  (princ "A malicious hydra with ")
+  (princ (monster-health m))
+  (princ " heads."))
+
+(defmethod monster-hit ((m hydra) x)
+  (decf (monster-health m) x)
+  (if (monster-dead m)
+      (princ "The corpse of the fully decapitated hydra falls to the floor!")
+      (progn (princ "You lop off ")
+	     (princ x)
+	     (princ " of the hydra's heads! "))))
+
+(defmethod monster-attack ((m hydra))
+  (let ((x (randval (ash (monster-health m) -1))))
+    (princ "A hydra attacks you with ")
+    (princ x)
+    (princ " of its heads! It also grows back one more head!" )
+    (incf (monster-health m))
+    (decf *player-health* x)))
+
+;; The slimy slime mold! When it attacks you, it wraps itself around
+;; you and you are immobolised.
+(defstruct (slime-mold (:include monster))
+  (sliminess (randval 5)))
+(push #'make-slime-mold *monster-builders*)
+
+(defmethod monster-show ((m slime-mold))
+  (princ "A slime mold with a sliminess of ")
+  (princ (slime-mold-sliminess m)))
+
+(defmethod monster-attack ((m slime-mold))
+  (let ((x (randval (slime-mold-sliminess m))))
+    (princ "A slime mold wraps around your legs and decreases your agility by ")
+    (princ x)
+    (princ "!")
+    (decf *player-agility* x)
+    (when (zerop (random 2))
+      (princ "It also squirts in your face, taking away a health point! ")
+      (decf *player-health*))))
+
+;; The cunning brigand. The smartest of all your foes, he uses his
+;; whip or slingshot. His attacks are not powerful, but they are
+;; consistent, knocking two points off of you every round.
+(defstruct (cunning-brigand (:include monster)))
+(push #'make-cunning-brigand *monster-builders*)
+
+(defmethod monster-attack ((m cunning-brigand))
+  (let ((x (max *player-health* *player-agility* *player-strength*)))
+    (cond ((= x *player-health*)
+	   (princ "A brigand hits your with his slingshot, taking off two health points!")
+	   (decf *player-health* 2))
+	  ((= x *player-agility*)
+	   (princ "A brigand catches your leg with his whip, taking off 2 agility points!")
+	   (decf *player-agility* 2))
+	  ((= x *player-strength*)
+	   (princ "A brigand cuts your arm with his whip, taking off 2 strength points!")
+	   (decf *player-strength* 2)))))
