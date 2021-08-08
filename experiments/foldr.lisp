1b124fb811480b6cc5f23dbcfa02a43168f7ba47
Jay Morgan <jaymiles17@gmail.com>
2021-08-08 13:53:54 +0100

    Add more foldr examples

--- experiments/foldr.lisp
+++ experiments/foldr.lisp
@@ -9,3 +9,50 @@
 
 (defun product (lst)
   (foldr #'* 1 lst))
+
+(defun anytrue (lst)
+  (foldr #'logior 0 lst))
+
+(defun alltrue (lst)
+  (foldr #'logand 1 lst))
+
+(defun my/append (a b)
+  (foldr #'cons b a))
+
+(defun my/length (lst)
+  (labels ((my/count (a n)
+	     (declare (ignore a))
+	     (incf n)))
+    (foldr #'my/count 0 lst)))
+
+(defun doubleall (lst)
+  (labels ((doubleandcons (n lst)
+	     (cons (* 2 n) lst)))
+    (foldr #'doubleandcons nil lst)))
+
+;; Tests
+
+(sum '()) ;; => 0
+(sum '(1 2 3)) ;; => 6
+
+(product '()) ;; => 1
+(product '(1 2 3)) ;; => 6
+
+(anytrue '(0 0 0)) ;; => 0
+(anytrue '(1 0 0)) ;; => 1
+(anytrue '(1 1 1)) ;; => 1
+(anytrue '()) ;; => 0
+
+(alltrue '(0 1 0)) ;; => 0
+(alltrue '(1 1 0)) ;; => 0
+(alltrue '()) ;; => 1
+(alltrue '(1 1 1)) ;; => 1
+
+(my/append '(1 2) '(3 4)) ;; => (1 2 3 4)
+(my/append '() '()) ;; => NIL
+
+(my/length '(1 2 3 4)) ;; => 4
+(my/length '()) ;; => 0
+
+(doubleall '(1 2 3 4)) ;; => (2 4 6 8)
+(doubleall '()) ;; => NIl
