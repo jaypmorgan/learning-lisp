b2316c256a36851adcded18f04e07985bdf015cc
Jay Morgan <jaymiles17@gmail.com>
2021-08-31 22:00:46 +0200

    Add more answers to chapter 6

--- common-lisp/chapter-6.lisp
+++ common-lisp/chapter-6.lisp
@@ -156,3 +156,160 @@
 ;; result.
 
 (compare '(small red meta cube -vs- red plastic small cube))
+
+;; 6.28 Set the global variable PRODUCE to this list:
+
+(defparameter produce
+  '((apple	. fruit)
+    (celery	. veggie)
+    (banana	. fruit)
+    (lettuce	. veggie)))
+
+;; Now write down the results of the following expressions:
+
+(assoc 'banana produce) ;; => (BANANA . FRUIT)
+(rassoc 'fruit produce) ;; => (APPLE . FRUIT)
+(assoc 'lettuce produce) ;; => (LETTUCE . VEGGIE)
+(rassoc 'veggie produce) ;; => (CELERY . VEGGIE)
+
+;; 6.30 Make a table called books of five books and their authors. The
+;; first entry might be (WAR-AND-PIECE LEO-TOLSTOY).
+
+(defparameter books
+  '((war-and-piece leo-tolstoy)
+    (the-catcher-in-the-rye j-d-salinger)
+    (frankenstein mary-shelly)
+    (hitchhikers-guide-to-the-galaxy douglas-adams)
+    (the-waves virginia-wolf)))
+
+;; 6.31 Write the function WHO-WROTE that takes the name of a book as
+;; input and returns the book's author.
+
+(defun who-wrote (book-name)
+  (second (assoc book-name books)))
+
+(who-wrote 'war-and-piece) ;; => LEO-TOLSTOY
+(who-wrote 'frankenstein) ;; => MARY-SHELLY
+(who-wrote 'common-lisp) ;; => NIL
+
+;; Mini-keyboard exercise
+
+;; 6.35 In this problem we will simulate the
+;; behaviour of a very simple-minded creature, /Nerdus Americanis/
+;; (also known as /Computerus Hackerus/). This creature has only five
+;; states: Sleeping, Eating, Waiting-for-a-computer, Programming, and
+;; Debugging. Its behaviour is cyclic: After it sleeps it always eats,
+;; after it eats it always waits for a computer, and so on, until
+;; after debugging it goes back to sleep for a while.
+;;
+;; a. What type of data structure would be useful for representing the
+;; connection between a state and its successor? Write such a data
+;; structure for the five-state cycle given above, and store it in a
+;; global variable called NERD-STATES.
+
+;; a simple table structure should work fine, though if it wanted
+;; something more complex, we could set the cdr of the end of a list
+;; of states to the start of the list, making a cycle.
+(defparameter nerd-states
+  '((sleeping eating)
+    (eating waiting-for-a-computer)
+    (waiting-for-a-computer programming)
+    (programming debugging)
+    (debugging sleeping)))
+
+;; b. Write a function NERDUS that takes the name of a state as input
+;; and uses the data structure you designed to determine the next
+;; state the creature will be in. (NERDUS 'SLEEPING) should return
+;; EATING, for example. (NERDUS 'DEBUGGING) should return SLEEPING.
+
+(defun nerdus (state)
+  (cadr (assoc state nerd-states)))
+
+(nerdus 'sleeping)
+(nerdus 'debugging)
+
+;; c. What is the result of (NERDUS 'PLAYING-GUITAR)?
+
+(nerdus 'playing-guitar) ;; => NIL
+
+;; d. When /Nerdus Americanis/ ingests too many stimulants (caffeine
+;; overdose), it stops sleeping. After finishing Debugging, it
+;; immediately goes on to state Eating. Write a function
+;; SLEEPLESS-NERD that works just like NERDUS except it never
+;; sleeps. Your function should refer to the global variable
+;; NERD-STATES, as NERDUS does.
+
+(defun sleepless-nerd (state)
+  (let ((next-state (nerdus state)))
+    (if (eq next-state 'sleeping)
+	(nerdus 'sleeping)
+	next-state)))
+
+(sleepless-nerd 'sleeping) ;; => EATING
+(sleepless-nerd 'eating) ;; => WAITING-FOR-A-COMPUTER
+
+;; e. Exposing /Nerdus Americanis/ to extreme amounts of chemical
+;; stimulants produces pathological behaviour. Instead of an orderly
+;; advance to its next state, the create advances two states. For
+;; example, it goes from Eating directly to Programming, and from
+;; there to Sleeping. Write a function NERD-ON-CAFFEINE that exhibits
+;; this unusual pathology. Your function should use the same table as
+;; NERDUS.
+
+(defun nerd-on-caffeine (state)
+  (nerdus (nerdus state)))
+
+(nerd-on-caffeine 'eating)
+(nerd-on-caffeine 'programming)
+
+;; Review exercises
+;;
+;; 6.36 Write a function to swap the first and last elements of any
+;; list. (SWAP-FIRST-LAST '(YOU CANT BUY LOVE)) should return (LOVE
+;; CANT BUY YOU).
+
+(defun swap-first-last (lst)
+  (let ((first-el (car lst))
+	(last-el (car (last lst))))
+    `(,last-el ,@(remove last-el (cdr lst)) ,first-el)))
+
+(swap-first-last '(you cant buy love))
+
+;; 6.37 ROTATE-LEFT and ROTATE-RIGHT are functions that rotate the
+;; element of a list. (ROTATE-LEFT '(A B C D E)) returns (B C D E A),
+;; whereas ROTATE-RIGHT returns (E A B C D). Write these functions.
+
+(defun rotate-right (lst)
+  (append (cdr lst) (list (car lst))))
+
+(defun rotate-left (lst)
+  (append (last lst) (remove (car (last lst)) lst)))
+
+(rotate-right '(a b c d e))
+(rotate-left '(a b c d e))
+
+;; 6.38 Give an example of two sets X and Y such that (SET-DIFFERENCE
+;; X Y) equals (SET-DIFFERENCE Y X). Also give an example in which the
+;; set differences are not equal.
+
+(eq (set-difference nil nil) (set-difference nil nil))
+(eq (set-difference '(1) nil) (set-difference nil '(1)))
+
+;; 6.40 Show how to transform the list (A B C D) into a table so that
+;; the ASSOC function using the table gives the same results as MEMBER
+;; using the list.
+
+(defparameter table-transform
+  '((A . (B C D))
+    (B . (C D))
+    (C . (D))
+    (D . nil)))
+
+(member 'A '(A B C D))
+(member 'B '(A B C D))
+(member 'C '(A B C D))
+(member 'D '(A B C D))
+(assoc 'A table-transform)
+(assoc 'B table-transform)
+(assoc 'C table-transform)
+(assoc 'D table-transform)
