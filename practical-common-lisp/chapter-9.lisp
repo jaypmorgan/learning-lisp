eb8256c787e37c0aec0a53241756ea7c74452274
Jay Morgan <jaymiles17@gmail.com>
2021-09-20 00:08:44 +0200

    Add chapter 9 practical

---
+++ practical-common-lisp/chapter-9.lisp
@@ -0,0 +1,43 @@
+;; Practical: Building a Unit Test Framework
+(defvar *test-name* nil)
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+
+(defmacro check (&body forms)
+  `(progn
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro with-gensyms (vars &body body)
+  `(let ,(loop for var in vars collect `(,var (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+(deftest test-+ ()
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -4)))
+
+(deftest test-* ()
+  (check
+    (= (* 2 2) 4)
+    (= (* 3 5) 15)))
+
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+
+(test-arithmetic)
