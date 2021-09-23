88cbe5359863ea0c87ef42674adf2902807bfab2
Jay Morgan <jaymiles17@gmail.com>
2021-09-23 23:26:44 +0200

    Add examples of defmethod and defgeneric

---
+++ practical-common-lisp/chapter-16.lisp
@@ -0,0 +1,20 @@
+(defgeneric withdraw (account amount)
+  (:documentation "Withdraw the specified amount from the account.
+Signal an error if the current balance is less than the amount."))
+
+(defmethod withdraw ((account bank-account) amount)
+  (when (< (balance account) amount)
+    (error "Account overdrawn."))
+  (decf (balance account) amount))
+
+(defmethod withdraw :before ((account checking-account) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (withdraw (overdraft-account account) overdraft)
+      (incf (balance account) overdraft))))
+
+(defmethod withdraw ((account (eql *account-of-bank-president*)) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (incf (balance account) (embezzle *bank* overdraft)))
+    (call-next-method)))
