271a422d1005845ab310eef65b34256f42914f90
Jay Morgan <jaymiles17@gmail.com>
2021-09-24 18:20:42 +0200

    Add bank-account class

--- practical-common-lisp/chapter-16.lisp
+++ practical-common-lisp/chapter-16.lisp
@@ -1,3 +1,42 @@
+(defparameter *account-numbers* 1)
+
+(defclass bank-account ()
+  ((customer-name
+    :initarg :customer-name
+    :initform (error "Must supply a customer name.")
+    :accessor customer-name
+    :documentation "Customer's name")
+   (balance
+    :initarg :balance
+    :initform 0
+    :reader balance
+    :documentation "Current account balance")
+   (account-number
+    :initform (incf *account-numbers*)
+    :reader account-number
+    :documentation "Account number, unique within a bank.")
+   (account-type
+    :reader account-type
+    :documentation "Type of account, one of :gold, :silver, or :bronze.")))
+
+(defmethod initialize-instance :after ((account bank-account) &key)
+  (let ((balance (slot-value account 'balance)))
+    (setf (slot-value account 'account-type)
+	  (cond
+	    ((>= balance 100000) :gold)
+	    ((>= balance 50000) :silver)
+	    (t :bronze)))))
+
+(defmethod assess-low-balance-penalty ((account bank-account))
+  (with-slots (balance) account
+    (when (< balance *minimum-balance*)
+      (decf balance (* balance .01)))))
+
+(defgeneric balance (account))
+
+(defmethod balance ((account bank-account))
+  (slot-value account 'balance))
+
 (defgeneric withdraw (account amount)
   (:documentation "Withdraw the specified amount from the account.
 Signal an error if the current balance is less than the amount."))
@@ -18,3 +57,10 @@ Signal an error if the current balance is less than the amount."))
     (when (plusp overdraft)
       (incf (balance account) (embezzle *bank* overdraft)))
     (call-next-method)))
+
+;; -- application
+
+(defparameter *account*
+  (make-instance 'bank-account
+		 :customer-name "John Doe"
+		 :balance 1000))
